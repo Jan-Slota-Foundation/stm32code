c2ac8cd38156b6e877d97f260697fcfd36de10a1
jakubkapitulcin <kapitulcinjakub1@gmail.com>
2024-04-06 13:35:16 +0200

    buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-#define LED_PIN 13
-#define BUTTON_LEFT 15
-#define BUTTON_MIDDLE 16
-#define BUTTON_RIGHT 17
+// pins
+#define BUTTON_LED 12
+#define SERIAL_LED 13
+#define BUTTON_LEFT A1
+#define BUTTON_MIDDLE A2
+#define BUTTON_RIGHT A3
+
+// led states
+#define LED_ON LOW
+#define LED_OFF HIGH
+
+/*
+  LEDS ARE CATODE CONNECTED
+*/
 
 /*
 log for printing/reading from serial:
@@ -22,25 +32,41 @@ log for printing/reading from serial:
   Serial.print(" is ");
   Serial.println(myFunction(x, y));
 
+// log for reading buttons:
+while (digitalRead(BUTTON_LEFT));     // wait until S1 (low active)
+  Serial.printf("%i\n", digitalRead(BUTTON_LEFT));
+  delay(1000);
+  if (!digitalRead(BUTTON_LEFT)) {
+    digitalWrite(BUTTON_LED, LED_ON);
+    delay(1000);
+  } else {
+    digitalWrite(BUTTON_LED, LED_OFF);
+    delay(1000);
+  }
 */
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
-  digitalWrite(LED_PIN, HIGH);
+  pinMode(SERIAL_LED, OUTPUT);
+  digitalWrite(SERIAL_LED, LED_OFF);
+
+  pinMode(BUTTON_LED, OUTPUT);
+
+  pinMode(BUTTON_LEFT, INPUT);
+  // pinMode(BUTTON_MIDDLE, INPUT_PULLDOWN);
+  // pinMode(BUTTON_RIGHT, INPUT_PULLDOWN);
+
+  digitalWrite(BUTTON_LED, LED_OFF);
+
   Serial.begin(9600);
+  digitalWrite(SERIAL_LED, LED_ON);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
 }
 
 // put function definitions here:
