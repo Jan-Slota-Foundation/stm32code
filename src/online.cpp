042d583937702b9e871c6c995eea3b0f497ee60c
jakubkapitulcin <kapitulcinjakub1@gmail.com>
2024-04-07 10:52:37 +0200

    cleanup

--- src/online.cpp
+++ src/online.cpp
@@ -1,39 +1,55 @@
 #include "online.h"
 #include <Arduino.h>
 
+// 0 means notes, 1 means tempo
+int reading_state = 0;
+int found_terminate = -1;
+
+int songIndex = 0;
+
+Song s;
+Note currentNote;
+
 void handle_online(int r1, int r2, State *state) {
-    Serial.println("Enter notes:");
-    Song s;
-    Note currentNote;
-    int songIndex = 0;
-    digitalWrite(LED_2, LED_ON);
-
-    while (currentNote.note != TERMINATE) {
-    while (Serial.available() < 1) {delay(100);}
-
     char *buffer = new char[3];
-    currentNote.note = Serial.readBytes(buffer, 3);
-    currentNote.note = buffer[0] - '0';
+    if (found_terminate == -1) {
+        Serial.println("Enter notes:");
+        digitalWrite(LED_2, LED_ON);
+        s = Song();
+        songIndex = 0;
+        currentNote = Note();
+        found_terminate = 0;
+    } else if (found_terminate == 0) {
+        if (Serial.available() > 1) {
+            currentNote.note = Serial.readBytes(buffer, 3);
+            currentNote.note = buffer[0] - '0';
 
-    currentNote.octave = buffer[1] - '0';
+            currentNote.octave = buffer[1] - '0';
 
-    currentNote.duration = buffer[2] - '0';
+            currentNote.duration = buffer[2] - '0';
 
-    s.melody[songIndex] = currentNote;
-    songIndex++;
+            s.melody[songIndex] = currentNote;
+            songIndex++;
+            if (currentNote.note == TERMINATE) {
+                found_terminate = 1;
+            }
+        }
+    } else if (found_terminate == 1) {
+        if (Serial.available() > 1) {
+            s.tempo = Serial.readBytes(buffer, 3);
+            s.tempo = buffer[0] - '0';
+            s.tempo = s.tempo * 2.5;
+            //s.tempo = 128;
+            digitalWrite(LED_3, LED_OFF);
+            digitalWrite(LED_2, LED_OFF);
+            playSong(s);
+            delay(1000);
+            noTone(BUZZER_PIN);
+            Serial.println("Song played");
+            found_terminate = -1;
+        }
     }
-
-    while (Serial.available() < 1) {}
-
-    // s.tempo = Serial.parseInt();
-    // Serial.println(s.tempo);
-    s.tempo = 128;
-    digitalWrite(LED_3, LED_OFF);
-    digitalWrite(LED_2, LED_OFF);
-    playSong(s);
-    delay(1000);
-    noTone(BUZZER_PIN);
-    Serial.println("Song played");
+    delete buffer;
 }
 
 void finish_online_state() {
